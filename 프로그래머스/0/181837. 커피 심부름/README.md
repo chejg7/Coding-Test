aae22a874b15e339283c1ad0b3ac108a4f716499
Alex <70855337+chejg7@users.noreply.github.com>
2025-03-08 20:35:58 +0800

    [level 0] Title: 커피 심부름, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/README.md
+++ 프로그래머스/0/181837. 커피 심부름/README.md
@@ -1,10 +1,10 @@
 # [level 0] 커피 심부름 - 181837 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181837) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181837?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.39 ms
+메모리: 10.3 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 08일 20:34:38
+2025년 03월 08일 20:35:56
 
 ### 문제 설명
 
--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,8 +1,19 @@
+# def solution(order):
+#     answer = 0
+#     for o in order:
+#         if o[:9] == "americano" or o[3:] == "americano" or o == "anything":
+#             answer += 4500
+#         else:
+#             answer += 5000
+#     return answer
+
+# 조건식을 훨씬 간단하게 쓸 수 있음
+
 def solution(order):
     answer = 0
-    for o in order:
-        if o[:9] == "americano" or o[3:] == "americano" or o == "anything":
-            answer += 4500
-        else:
-            answer += 5000
+    for want in order:
+        if 'latte' in want:
+            answer += 500
+        answer += 4500
+
     return answer
