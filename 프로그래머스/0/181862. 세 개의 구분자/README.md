0c4e2f1d3f3487629da9250cfcfb27260b3ab0d8
Alex <70855337+chejg7@users.noreply.github.com>
2025-03-13 13:41:16 +0800

    [level 0] Title: 세 개의 구분자, Time: 14.61 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/README.md
+++ 프로그래머스/0/181862. 세 개의 구분자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 세 개의 구분자 - 181862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862?language=python3) 
 
 ### 성능 요약
 
-메모리: 20.3 MB, 시간: 169.16 ms
+메모리: 22.4 MB, 시간: 14.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 13일 13:38:04
+2025년 03월 13일 13:41:14
 
 ### 문제 설명
 
--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,15 +1,21 @@
+# def solution(myStr):
+#     answer = []
+#     temp = ""
+#     for s in myStr:
+#         if s == "a" or s == "b" or s == "c":
+#             if len(temp) > 0:
+#                 answer.append(temp)
+#                 temp = ""
+#             else:
+#                 continue
+#         else:
+#             temp += s
+#     if len(temp) > 0:
+#         answer.append(temp)
+#     return answer if len(answer) > 0 else ["EMPTY"]
+
 def solution(myStr):
-    answer = []
-    temp = ""
-    for s in myStr:
-        if s == "a" or s == "b" or s == "c":
-            if len(temp) > 0:
-                answer.append(temp)
-                temp = ""
-            else:
-                continue
-        else:
-            temp += s
-    if len(temp) > 0:
-        answer.append(temp)
-    return answer if len(answer) > 0 else ["EMPTY"]
+    answer = [x for x in myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split() if x]
+    return answer if answer else ['EMPTY']
+
+# a, b, c를 빈칸으로 대치하고 이를 split하면 쉽게 풀 수 있음.
