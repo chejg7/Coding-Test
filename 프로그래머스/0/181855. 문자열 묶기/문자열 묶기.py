253c631de311978163c3c819bc4aa0f180e2d35c
Alex <70855337+chejg7@users.noreply.github.com>
2025-03-14 15:09:48 +0800

    [level 0] Title: 문자열 묶기, Time: 25.02 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,9 +1,19 @@
+# def solution(strArr):
+#     answer = {}
+#     for s in strArr:
+#         length = len(s)
+#         if length in answer:
+#             answer[length] += 1
+#         else:
+#             answer[length] = 1
+#     return max(answer.values())
+
 def solution(strArr):
-    answer = {}
-    for s in strArr:
-        length = len(s)
-        if length in answer:
-            answer[length] += 1
-        else:
-            answer[length] = 1
-    return max(answer.values())
+    d = {}
+
+    for i in strArr:
+        d[len(i)] = d.get(len(i), 0) + 1
+
+    return max(d.values())
+
+# 딕셔너리의 get 메소드를 사용하면 더 간단하게 작성할 수 있음.
