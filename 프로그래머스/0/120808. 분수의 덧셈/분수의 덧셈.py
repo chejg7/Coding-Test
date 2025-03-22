569c9268c5db02086466701925a6ac0c1af04411
Alex <70855337+chejg7@users.noreply.github.com>
2025-03-22 16:41:48 +0800

    [level 0] Title: 분수의 덧셈, Time: 14.37 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,13 +1,35 @@
-import math
+# import math
 
-def solution(numer1, denom1, numer2, denom2):
-    # lcm = math.lcm(denom1, denom2)
-    # numer3 = numer1 * (lcm // denom1) + numer2 * (lcm // denom2)
-    # denom3 = lcm
-    # gcd = math.gcd(numer3, lcm)
+# def solution(numer1, denom1, numer2, denom2):
+#     # lcm = math.lcm(denom1, denom2)
+#     # numer3 = numer1 * (lcm // denom1) + numer2 * (lcm // denom2)
+#     # denom3 = lcm
+#     # gcd = math.gcd(numer3, lcm)
     
-    numer3 = numer1 * denom2 + numer2 * denom1
-    denom3 = denom1 * denom2
-    gcd = math.gcd(numer3, denom3)
+#     numer3 = numer1 * denom2 + numer2 * denom1
+#     denom3 = denom1 * denom2
+#     gcd = math.gcd(numer3, denom3)
         
-    return [numer3 // gcd, denom3 // gcd]
+#     return [numer3 // gcd, denom3 // gcd]
+
+# 최소공배수는 math 모듈에 없어서 최대공약수만 모듈을 사용해서 구함
+# 다른 풀이 중 최대공약수를 아래와 같이 구하는 방식도 있어서 참고함
+
+def solution(denum1, num1, denum2, num2):
+    answer = []
+    s = 0
+
+    denum0 = (denum1*num2) +(denum2*num1)
+    num0 = num1*num2
+
+    for i in range(min(denum0,num0),0,-1):
+        if denum0%i == 0 and num0%i == 0:
+            s = i
+            break
+
+    denum0 /= s
+    num0 /= s
+    answer.append(denum0)
+    answer.append(num0)
+
+    return answer
