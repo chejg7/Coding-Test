7317d846c221f403f65f5d583f583a4a23a93673
Alex <70855337+chejg7@users.noreply.github.com>
2025-04-01 20:58:07 +0800

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/README.md
+++ 프로그래머스/0/120848. 팩토리얼/README.md
@@ -1,10 +1,10 @@
 # [level 0] 팩토리얼 - 120848 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120848) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120848?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 0.01 ms
+메모리: 9.11 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 01일 20:54:57
+2025년 04월 01일 20:58:05
 
 ### 문제 설명
 
--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,8 +1,18 @@
-from math import factorial as f
+# from math import factorial as f
+
+# def solution(n):
+#     i = 0
+#     while f(i) <= n:
+#         i += 1
+#         answer = i
+#     return answer - 1
+
+# math 함수를 임포트하지 않고 팩토리얼 본연의 의미도 살리고 계산 횟수도 줄일 수 있음. 
 
 def solution(n):
-    i = 0
-    while f(i) <= n:
-        i += 1
-        answer = i
+    answer = 1
+    factorial = 1
+    while factorial <= n:
+        answer += 1
+        factorial = factorial * answer
     return answer - 1
