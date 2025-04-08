529f4be335806aef6618a4545a036139afc9d56a
Alex <70855337+chejg7@users.noreply.github.com>
2025-04-08 11:25:21 +0800

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,15 +1,21 @@
+# def solution(my_string):
+#     s_list = my_string.split(' ')
+#     answer = 0
+#     i = 0
+#     while i < len(s_list):
+#         if s_list[i] == '-':
+#             i += 1
+#             answer -= int(s_list[i])
+#         elif s_list[i] == '+':
+#             i += 1
+#             answer += int(s_list[i])
+#         else:
+#             answer += int(s_list[i])
+#         i += 1
+#     return answer
+
+# replace 함수를 사용해서 음수로 변환한뒤 모두 더하는 방법을 사용하면 가장 깔끔하게 풀 수 있음.
+# eval()함수를 사용하는 사람도 있었으나 이는 문자열로 된 코드를 곧바로 실행해버리기 때문에 보안상 매우 위험한 행동임. 
+
 def solution(my_string):
-    s_list = my_string.split(' ')
-    answer = 0
-    i = 0
-    while i < len(s_list):
-        if s_list[i] == '-':
-            i += 1
-            answer -= int(s_list[i])
-        elif s_list[i] == '+':
-            i += 1
-            answer += int(s_list[i])
-        else:
-            answer += int(s_list[i])
-        i += 1
-    return answer
+    return sum(int(i) for i in my_string.replace(' - ', ' + -').split(' + '))
