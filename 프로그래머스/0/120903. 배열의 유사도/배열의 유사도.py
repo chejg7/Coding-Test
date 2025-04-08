1c25e83f1e6276e1328bbb9cde7ca6d3a7d1e585
Alex <70855337+chejg7@users.noreply.github.com>
2025-04-08 11:29:26 +0800

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,6 +1,11 @@
+# def solution(s1, s2):
+#     answer = 0
+#     for s in s1:
+#         if s in s2:
+#             answer += 1
+#     return answer
+
+# 교집합으로 생각하면 매우 쉽게 풀 수 있음. 
+
 def solution(s1, s2):
-    answer = 0
-    for s in s1:
-        if s in s2:
-            answer += 1
-    return answer
+    return len(set(s1) & set(s2))
