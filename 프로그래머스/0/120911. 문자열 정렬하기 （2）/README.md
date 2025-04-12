0e26e3585017ceb205878c0ed123d707b5e3ea0a
Alex <70855337+chejg7@users.noreply.github.com>
2025-04-12 15:09:30 +0800

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/README.md
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 정렬하기 (2) - 120911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120911?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.19 MB, 시간: 0.03 ms
+메모리: 9.24 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 12일 15:08:38
+2025년 04월 12일 15:09:27
 
 ### 문제 설명
 
--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,10 +1,15 @@
+# def solution(my_string):
+#     answer = ''
+#     upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#     lower = 'abcdefghijklmnopqrstuvwxyz'
+#     for s in my_string:
+#         if s in upper:
+#             answer += lower[upper.index(s)]
+#         else:
+#             answer += s
+#     return ''.join(sorted(answer))
+
+# lower 메소드를 사용하면 매우 쉽게 풀 수 있음.
+
 def solution(my_string):
-    answer = ''
-    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    lower = 'abcdefghijklmnopqrstuvwxyz'
-    for s in my_string:
-        if s in upper:
-            answer += lower[upper.index(s)]
-        else:
-            answer += s
-    return ''.join(sorted(answer))
+    return ''.join(sorted(my_string.lower()))
