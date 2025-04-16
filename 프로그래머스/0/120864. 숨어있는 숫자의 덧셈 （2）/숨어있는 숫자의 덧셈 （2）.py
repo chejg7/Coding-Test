a9a415e2323b4ae14f43f54520737ece389b2b6d
Alex <70855337+chejg7@users.noreply.github.com>
2025-04-16 11:28:03 +0800

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.04 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,10 +1,16 @@
+# def solution(my_string):
+#     answer = 0
+#     temp = '0'
+#     for s in my_string:
+#         if s in '0123456789':
+#             temp += s
+#         else:
+#             answer += int(temp)
+#             temp = '0'
+#     return answer + int(temp)
+
+# isdigit()함수와 숫자 외의 문자를 모두 공백으로 바꿔버리는 방법을 사용하면 매우 간단하게 해결할 수 있음. 
+
 def solution(my_string):
-    answer = 0
-    temp = '0'
-    for s in my_string:
-        if s in '0123456789':
-            temp += s
-        else:
-            answer += int(temp)
-            temp = '0'
-    return answer + int(temp)
+    s = ''.join(i if i.isdigit() else ' ' for i in my_string)
+    return sum(int(i) for i in s.split())
