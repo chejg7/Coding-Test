485235ada49ee9d7af96118744bca026b2ac3621
Alex <70855337+chejg7@users.noreply.github.com>
2025-06-12 23:36:01 +0800

    Update 이어 붙인 수.py

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,4 +1,11 @@
+# def solution(num_list):
+#     answer1 = int(''.join(str(num) for num in num_list if num % 2 == 1))
+#     answer2 = int(''.join(str(num) for num in num_list if num % 2 == 0))
+#     return answer1 + answer2
+
+# 가독성을 고려하여 수정한 버전
+
 def solution(num_list):
-    answer1 = int(''.join(str(num) for num in num_list if num % 2 == 1))
-    answer2 = int(''.join(str(num) for num in num_list if num % 2 == 0))
-    return answer1 + answer2
+    odd = ''.join(str(num) for num in num_list if num % 2 == 1)
+    even = ''.join(str(num) for num in num_list if num % 2 == 0)
+    return int(odd) + int(even)
