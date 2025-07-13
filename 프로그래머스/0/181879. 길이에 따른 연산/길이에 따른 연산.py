357be6177367e2b6ff7312bea84c868e14e281d2
Alex <70855337+chejg7@users.noreply.github.com>
2025-07-13 19:25:56 +0800

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,14 +1,16 @@
+from math import prod
+
 def solution(num_list):
-    res1, res2 = 0, 1
-    if len(num_list) > 10:
-        for num in num_list:
-            res1 += num
-        return res1
-    else:
-        for num in num_list:
-            res2 *= num
-        return res2
+    # res1, res2 = 0, 1
+    # if len(num_list) > 10:
+    #     for num in num_list:
+    #         res1 += num
+    #     return res1
+    # else:
+    #     for num in num_list:
+    #         res2 *= num
+    #     return res2
     
-    # return sum(num_list) if len(num_list) > 10 else prod(num_list)
+    return sum(num_list) if len(num_list) > 10 else prod(num_list)
 
     
