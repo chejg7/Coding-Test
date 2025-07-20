d42c153ce9e83493da4f4e39b754414e8052c890
Alex <70855337+chejg7@users.noreply.github.com>
2025-07-20 22:07:07 +0800

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,3 +1,3 @@
 def solution(myString):
-    
-    return ''.join('A' if s.lower() == 'a' else s.lower() for s in myString)
+    # return ''.join('A' if s.lower() == 'a' else s.lower() for s in myString)
+    return myString.lower().replace('a', 'A')
