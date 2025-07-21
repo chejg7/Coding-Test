531ef3a1695fd3d9e0cd3b6e9ce5e3cc2273f751
Alex <70855337+chejg7@users.noreply.github.com>
2025-07-22 04:46:05 +0800

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,9 +1,20 @@
+# def solution(a, b, c):
+    # n1, n2, n3 = sorted([a, b, c])
+    # if n1 == n2 and n2 == n3:
+    #     answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
+    # elif (n1 == n2 and n2 != n3) or (n1 != n2 and n2 == n3):
+    #     answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
+    # else:
+    #     answer = a + b + c
+    # return answer
+
+    # set을 활용해서 동일값만 남길 수 있음
+    
 def solution(a, b, c):
-    n1, n2, n3 = sorted([a, b, c])
-    if n1 == n2 and n2 == n3:
-        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
-    elif (n1 == n2 and n2 != n3) or (n1 != n2 and n2 == n3):
-        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
+    check=len(set([a,b,c]))
+    if check==1:
+        return 3*a*3*(a**2)*3*(a**3)
+    elif check==2:
+        return (a+b+c)*(a**2+b**2+c**2)
     else:
-        answer = a + b + c
-    return answer
+        return (a+b+c)
